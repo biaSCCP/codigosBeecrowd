8182f22eed7f1860a9b94cd4f25089c6a1810e19
Bianca Gouveia <160188183+biaSCCP@users.noreply.github.com>
2025-03-04 17:15:39 +0000

    li dois valores inteiros e depois os somei

---
+++ problemas _beecrowd/problema1000.java
@@ -0,0 +1,24 @@
+/**
+ Bem-vindo ao beecrowd!
+
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". 
+Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!".
+ */
+
+
+import java.io.IOException;
+public class problema1000 {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+    System.out.println("Hello World!"); 
+ 
+    }
+ 
+}
